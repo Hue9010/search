0acff5216e83b4923dffa0f9eb1ebb67b4ca8968
hue9010 <hue9010@gmail.com>
2023-03-22 07:55:57 +0900

    add 인기 검색어 캐시 적용

--- src/main/java/com/hue/search/service/SearchKeywordService.java
+++ src/main/java/com/hue/search/service/SearchKeywordService.java
@@ -2,6 +2,8 @@ package com.hue.search.service;
 
 import com.hue.search.model.SearchKeyword;
 import com.hue.search.repository.SearchKeywordRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,12 +18,14 @@ public class SearchKeywordService {
         this.searchKeywordRepository = searchKeywordRepository;
     }
 
+    @Cacheable(cacheNames = "keywords", key = "'top_10'")
     @Transactional(readOnly = true)
     public List<SearchKeyword> findPopularKeywords() {
         return searchKeywordRepository.findTop10ByOrderByCountDescUpdatedAtDesc();
     }
 
     @Transactional
+    @CacheEvict(cacheNames = "keywords", key = "'top_10'")
     public void hitKeyword(String searchKeyword) {
         searchKeywordRepository.findByKeyword(searchKeyword)
                 .ifPresentOrElse(
